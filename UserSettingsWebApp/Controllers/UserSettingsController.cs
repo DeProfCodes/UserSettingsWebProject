45dbc032760fac177a174936744642f34e05a2e4
Proficient Mkansi <de.prof.codes@gmail.com>
2023-10-14 22:33:02 +0200

    Add project files.

---
+++ UserSettingsWebApp/Controllers/UserSettingsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using UserSettingsSharedProject.Models;
+using UserSettingsSharedProject.ViewModels;
+using UserSettingsWebApp.Models;
+using UserSettingsWebApp.Services;
+
+namespace UserSettingsWebApp.Controllers
+{
+    public class UserSettingsController : Controller
+    {
+        private readonly IUserSettingsAPI _userSettingsApi;
+        private readonly ILogger<UserSettingsController> _logger;
+
+        public UserSettingsController(IUserSettingsAPI userSettingsApi, ILogger<UserSettingsController> logger)
+        {
+            _userSettingsApi = userSettingsApi;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userSettingsViewModel = new UserSettingsViewModel();
+            try
+            {
+                userSettingsViewModel.AllUserSettings = await _userSettingsApi.GetAllUserSettings();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"An error occured while retrieving all user settings, error = {ex.Message}");
+            }
+            return View(userSettingsViewModel);
+        }
+
+        public async Task<IActionResult> UserSettingDetails(int id)
+        {
+            var userSettingViewModel = new UserSetting();
+            try
+            {
+                userSettingViewModel = await _userSettingsApi.GetUserSetting(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occured while retrieving all user settings, error = {ex.Message}");
+            }
+            return PartialView(userSettingViewModel);
+        }
+
+        public async Task<IActionResult> UpdateUserSettingDetails(string payload)
+        {
+            var userSettingViewModel = new UserSetting();
+            try
+            {
+                var userSetting = await _userSettingsApi.UpdateUserSetting(payload);    
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occured while retrieving all user settings, error = {ex.Message}");
+                
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ UserSettingsWebApp/Services/UserSettingsAPI.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using UserSettingsSharedProject.Models;
+using UserSettingsShareProject.ViewModels;
+
+namespace UserSettingsWebApp.Services
+{
+    public class UserSettingsAPI : IUserSettingsAPI
+    {
+        private HttpClient client;
+
+        public UserSettingsAPI()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44359/api/");
+        }
+
+        public async Task<List<UserSetting>> GetAllUserSettings()
+        {
+            var result = await client.GetAsync("getAllUserSettings");
+            
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<List<UserSetting>>();
+                readTask.Wait();
+
+                var allUserSettings = readTask.Result;
+
+                return allUserSettings;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<UserSetting> GetUserSetting(int userSettingId)
+        {
+            var url = $"getUserSettingById?id={userSettingId}";
+
+            var result = await client.GetAsync(url);
+            
+            if (result.IsSuccessStatusCode)
+            {
+                var userSetting = await result.Content.ReadAsAsync<UserSetting>();
+
+                return userSetting;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> CreateUserSetting(string payload)
+        {
+            var url = $"createUserSetting?payload={payload}";
+
+            var result = await client.GetAsync(url);
+            var message = await result.Content.ReadAsAsync<string>();
+            
+            return message;
+        }
+
+        public async Task<string> UpdateUserSetting(string payload)
+        {
+            var url = $"updateUserSetting?payload={payload}";
+
+            var userSettingVm = JsonSerializer.Deserialize<UserSettingViewModel>(payload);
+            var userSetting = new UserSetting
+            {
+                Id = Convert.ToInt32(userSettingVm.Id),
+                SettingDescription = userSettingVm.SettingDescription,
+                DesktopTimeout = Convert.ToInt64(userSettingVm.DesktopTimeout),
+                TickerTimeout = Convert.ToInt64(userSettingVm.TickerTimeout),
+                SyncTimeout = Convert.ToInt64(userSettingVm.SyncTimeout),
+                ScreenSaverTimeout = Convert.ToInt64(userSettingVm.ScreenSaverTimeout),
+                PopupTimeout = Convert.ToInt64(userSettingVm.PopupTimeout),
+            };
+
+            var result = await client.GetAsync(url);
+            result = await client.PutAsJsonAsync(url, userSetting);
+
+            var message = await result.Content.ReadAsAsync<string>();
+
+            return message;
+        }
+
+        public async Task<string> DeleteUserSetting(int userSettingId)
+        {
+            var url = $"deleteUserSettingBy?id={userSettingId}";
+
+            var result = await client.GetAsync(url);
+            var message = await result.Content.ReadAsAsync<string>();
+
+            return message;
+        }
+    }
+}
